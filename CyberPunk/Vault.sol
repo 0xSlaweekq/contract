93c92a1fb276030dc3c46ed770f17c057a17770c
Narconik <plakidin.vyacheslav@mail.ru>
2022-03-13 18:54:14 +0700

    1

--- CyberPunk/Vault.sol
+++ CyberPunk/Vault.sol
@@ -1,27 +1,7 @@
-/**
- *Submitted for verification at testnet.snowtrace.io on 2022-03-13
-*/
-
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
 interface IERC20 {
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() external view returns (string memory);
-
-    /**
-     * @dev Returns the symbol of the token.
-     */
-    function symbol() external view returns (string memory);
-
-    /**
-     * @dev Returns the decimals places of the token.
-     */
-    function decimals() external view returns (uint8);
-
     /**
      * @dev Returns the amount of tokens in existence.
      */
@@ -610,7 +590,7 @@ contract ReentrancyGuard {
 
 }
 
-contract MasterChef is Ownable, ReentrancyGuard {
+contract CyberBank is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeMath for uint16;
     using SafeMath for uint;
@@ -618,23 +598,35 @@ contract MasterChef is Ownable, ReentrancyGuard {
 
     // Info of each user.
     struct UserInfo {
-        uint256 amount;
-        uint256 rewardDebt;
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of tokens
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accTokenPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accTokenPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
     }
 
     // Info of each pool.
     struct PoolInfo {
-        IERC20 lpToken;
+        IERC20 lpToken;           // Address of LP token contract, zero represents HT pool.
         IERC20 rewardToken;
-        uint256 acc_token_precision;
-        uint256 amount;
-        uint256 rewardForEachBlock;
-        uint256 lastRewardBlock;
-        uint256 accTokenPerShare;
-        uint256 startBlock;
-        uint256 endBlock;
-        uint256 rewarded;
+        uint256 amount;     // How many LP tokens the pool has.
+        uint256 rewardForEachBlock;    //Reward for each block
+        uint256 lastRewardBlock;  // Last block number that token distribution occurs.
+        uint256 accTokenPerShare; // Accumulated token per share, times 1e12. See below.
+        uint256 startBlock; // Reward start block.
+        uint256 endBlock;  // Reward end block.
+        uint256 rewarded;// the total token has beed reward, including the dev and user harvest
     }
+
+    uint256 private constant ACC_TOKEN_PRECISION = 1e18;
 
     uint8 public constant ZERO = 0;
     uint16 public constant RATIO_BASE = 1000;
@@ -649,13 +641,13 @@ contract MasterChef is Ownable, ReentrancyGuard {
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event Harvest(address indexed user, uint256 indexed pid, uint256 amount);
     event HarvestAndRestake(address indexed user, uint256 indexed pid, uint256 amount);
-    event EmergencyStop(address indexed user);
+    event EmergencyStop(address indexed user, address to);
     event Add(uint256 rewardForEachBlock, IERC20 lpToken, bool withUpdate,
         uint256 startBlock, uint256 endBlock, bool withTokenTransfer);
-    event SetPoolInfo(uint256 pid, uint256 rewardsTotal, bool withUpdate, uint256 startBlock, uint256 endBlock);
-    event ClosePool(uint256 pid);
+    event SetPoolInfo(uint256 pid, uint256 rewardsOneBlock, bool withUpdate, uint256 startBlock, uint256 endBlock);
+    event ClosePool(uint256 pid, address payable to);
 
-    event AddRewardForPool(uint256 pid, uint256 addTokenPerBlock, bool withTokenTransfer);
+    event AddRewardForPool(uint256 pid, uint256 addTokenPerPool, bool withTokenTransfer);
 
     modifier validatePoolByPid(uint256 _pid) {
         require(_pid < poolInfo .length, "Pool does not exist");
@@ -679,17 +671,10 @@ contract MasterChef is Ownable, ReentrancyGuard {
         if (_withUpdate) {
             massUpdatePools();
         }
-        if (_rewardToken != IERC20(address(0))) {
-            _totalReward = _totalReward * 10 ** uint256(_rewardToken.decimals());
-        } else {
-            _totalReward = _totalReward * 10 **18;
-        }
         uint256 _rewardForEachBlock = _totalReward.div(_endBlock.sub(_startBlock));
-
         poolInfo.push(PoolInfo({
             lpToken : _lpToken,
             rewardToken: _rewardToken,
-            acc_token_precision: 10**uint256(_rewardToken.decimals()),
             amount : ZERO,
             rewardForEachBlock : _rewardForEachBlock,
             lastRewardBlock : block.number > _startBlock ? block.number : _startBlock,
@@ -706,7 +691,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
     }
 
     // Update the given pool's pool info. Can only be called by the owner. 
-    function setPoolInfo(uint256 _pid, uint256 _totalReward, bool _withUpdate, uint256 _startBlock, uint256 _endBlock) external validatePoolByPid(_pid) onlyOwner {
+    function setPoolInfo(uint256 _pid, uint256 _rewardForEachBlock, bool _withUpdate, uint256 _startBlock, uint256 _endBlock) external validatePoolByPid(_pid) onlyOwner {
         if (_withUpdate) {
             massUpdatePools();
         }
@@ -725,11 +710,10 @@ contract MasterChef is Ownable, ReentrancyGuard {
             }
             pool.endBlock = _endBlock;
         }
-        if (_totalReward > ZERO) {
-            uint256 _rewardForEachBlock = _totalReward.div(pool.endBlock.sub(pool.startBlock));
+        if (_rewardForEachBlock > ZERO) {
             pool.rewardForEachBlock = _rewardForEachBlock;
         }
-        emit SetPoolInfo(_pid, _totalReward, _withUpdate, _startBlock, _endBlock);
+        emit SetPoolInfo(_pid, _rewardForEachBlock, _withUpdate, _startBlock, _endBlock);
     }
 
     // Return reward multiplier over the given _from to _to block.
@@ -738,12 +722,6 @@ contract MasterChef is Ownable, ReentrancyGuard {
             return _to.sub(_from);
         }
         return ZERO;
-    }
-
-    /// @return APR for 1 year
-    function getAPR(uint256 _pid) public view validatePoolByPid(_pid) returns (uint256) {
-        PoolInfo storage pool = poolInfo[_pid];
-        return pool.rewardForEachBlock * 20*60*24*365 * 10**18 / pool.amount;
     }
 
     // Update reward variables of the given pool to be up-to-date.
@@ -776,11 +754,12 @@ contract MasterChef is Ownable, ReentrancyGuard {
         uint256 tokenReward = multiplier.mul(pool.rewardForEachBlock);
         if (tokenReward > ZERO) {
             uint256 poolTokenReward = tokenReward;
-            pool.accTokenPerShare = pool.accTokenPerShare.add(poolTokenReward.mul(pool.acc_token_precision).div(lpSupply));
+            pool.accTokenPerShare = pool.accTokenPerShare.add(poolTokenReward.mul(ACC_TOKEN_PRECISION).div(lpSupply));
         }
     }
 
-    function pendingReward(uint256 _pid, address _user) public view validatePoolByPid(_pid) returns (uint256 tokenReward) {
+    // View function to see pending on frontend.
+    function pendingTokens(uint256 _pid, address _user) public view validatePoolByPid(_pid) returns (uint256 tokenReward) {
         PoolInfo storage pool = poolInfo[_pid];
         if (_user == address(0)) {
             _user = msg.sender;
@@ -800,9 +779,9 @@ contract MasterChef is Ownable, ReentrancyGuard {
                 multiplier = getMultiplier(lastRewardBlock, block.number);
             }
             uint256 poolTokenReward = multiplier.mul(pool.rewardForEachBlock).div(RATIO_BASE);
-            accTokenPerShare = accTokenPerShare.add(poolTokenReward.mul(pool.acc_token_precision).div(lpSupply));
+            accTokenPerShare = accTokenPerShare.add(poolTokenReward.mul(ACC_TOKEN_PRECISION).div(lpSupply));
         }
-        tokenReward = user.amount.mul(accTokenPerShare).div(pool.acc_token_precision).sub(user.rewardDebt);
+        tokenReward = user.amount.mul(accTokenPerShare).div(ACC_TOKEN_PRECISION).sub(user.rewardDebt);
     }
 
     // Update reward vairables for all pools. Be careful of gas spending!
@@ -813,22 +792,22 @@ contract MasterChef is Ownable, ReentrancyGuard {
         }
     }
 
-    function deposit(uint256 _pid, uint256 _amount) external payable validatePoolByPid(_pid) {
+    // Deposit LP tokens to CyberBank
+    function deposit(uint256 _pid, uint256 _amount) external validatePoolByPid(_pid) payable {
         PoolInfo storage pool = poolInfo[_pid];
         require(block.number <= pool.endBlock, "this pool has ended!");
         require(block.number >= pool.startBlock, "this pool has not started!");
+        if (pool.lpToken == IERC20(address(0))) {//if pool is HT
+            require(_amount == msg.value, "msg.value must be equals to amount!");
+        }
         UserInfo storage user = userInfo[_pid][msg.sender];
         harvest(_pid, msg.sender);
         if (pool.lpToken != IERC20(address(0))) {
-            _amount = _amount * 10 ** uint256(pool.lpToken.decimals());
             pool.lpToken.safeTransferFrom(msg.sender, address(this), _amount);
-        } else {
-            _amount = _amount * 10**18;
-            require(_amount == msg.value, "msg.value must be equals to amount!");        
         }
         pool.amount = pool.amount.add(_amount);
         user.amount = user.amount.add(_amount);
-        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(pool.acc_token_precision);
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(ACC_TOKEN_PRECISION);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -836,20 +815,15 @@ contract MasterChef is Ownable, ReentrancyGuard {
         return _token == address(0);
     }
 
-    // Withdraw LP tokens from MasterChef.
-    function withdraw(uint256 _pid, uint256 _amount) external validatePoolByPid(_pid) {
+    // Withdraw LP tokens from CyberBank.
+    function withdraw(uint256 _pid, uint256 _amount) external validatePoolByPid(_pid) payable {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(block.number >= pool.startBlock, "this pool has not started!");
-        if (pool.lpToken != IERC20(address(0))) {
-            _amount = _amount * 10 **uint256(pool.lpToken.decimals());
-        } else if (pool.lpToken == IERC20(address(0))) {
-            _amount = _amount * 10**18;
-        }
         require(user.amount >= _amount, "withdraw: not good");
         harvest(_pid, msg.sender);
         user.amount = user.amount.sub(_amount);
-        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(pool.acc_token_precision);
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(ACC_TOKEN_PRECISION);
         pool.amount = pool.amount.sub(_amount);
 
         if (pool.lpToken != IERC20(address(0))) {
@@ -861,7 +835,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
     }
 
     //transfer HT
-    function transferMainnetToken(address payable _to, uint256 _amount) internal {
+    function transferMainnetToken(address payable _to, uint256 _amount) internal nonReentrant {
         _to.transfer(_amount);
     }
 
@@ -871,45 +845,26 @@ contract MasterChef is Ownable, ReentrancyGuard {
         }
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_to];
-        if(user.amount != 0) {
-            updatePool(_pid);
-            uint256 pending = user.amount.mul(pool.accTokenPerShare).div(pool.acc_token_precision).sub(user.rewardDebt);
-            if (pending > ZERO) {
-                success = true;
-                if (pool.rewardToken == IERC20(address(0))) {
-                    transferMainnetToken(payable(_to), pending);
-                } else {
-                    safeTransferTokenFromThis(pool.rewardToken, _to, pending);
-                }
-                pool.rewarded = pool.rewarded.add(pending);
-                user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(pool.acc_token_precision);
-            } else {
-                success = false;
-            }
-            emit Harvest(_to, _pid, pending);
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accTokenPerShare).div(ACC_TOKEN_PRECISION).sub(user.rewardDebt);
+        if (pending > ZERO) {
+            success = true;
+            safeTransferTokenFromThis(pool.rewardToken, _to, pending);
+            pool.rewarded = pool.rewarded.add(pending);
+            user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(ACC_TOKEN_PRECISION);
+        } else {
+            success = false;
         }
+        emit Harvest(_to, _pid, pending);
     }
 
-    function emergencyStop() public onlyOwner {
-        uint256 length = poolInfo.length;
-        for (uint256 pid = ZERO; pid < length; ++pid) {
-            closePool(pid);
-        }
-        emit EmergencyStop(msg.sender);
-    }
-
-    function closePool(uint256 _pid) public validatePoolByPid(_pid) onlyOwner {
+    function closePool(uint256 _pid, address payable _to) public validatePoolByPid(_pid) onlyOwner {
         PoolInfo storage pool = poolInfo[_pid];
-        if (block.timestamp < pool.endBlock) {
-            uint256 rewardDiff = pool.rewardForEachBlock.mul(pool.endBlock.sub(block.timestamp));
-            if (pool.rewardToken != IERC20(address(0))) {
-                pool.rewardToken.safeTransfer(_msgSender(), rewardDiff);
-            } else {
-                transferMainnetToken(_msgSender(), rewardDiff);
-            }
-        }
         pool.endBlock = block.number;
-        emit ClosePool(_pid);
+        if (_to == address(0)) {
+            _to = payable(msg.sender);
+        }
+        emit ClosePool(_pid, _to);
     }
 
     // Safe transfer token function, just in case if rounding error causes pool to not have enough tokens.
@@ -922,30 +877,24 @@ contract MasterChef is Ownable, ReentrancyGuard {
         }
     }
 
-    function addRewardForPool(uint256 _pid, uint256 _addTotalTokens, bool _withTokenTransfer) external validatePoolByPid(_pid) onlyOwner {
-        require(_addTotalTokens > ZERO, "add token must be greater than zero!");
+    // Add reward for pool from the current block or start block
+    function addRewardForPool(uint256 _pid, uint256 _addTokenPerPool, bool _withTokenTransfer) external validatePoolByPid(_pid) onlyOwner {
+        require(_addTokenPerPool > ZERO, "token amount must be greater than zero!");
         PoolInfo storage pool = poolInfo[_pid];
         require(block.number < pool.endBlock, "this pool has ended!");
         updatePool(_pid);
 
-        if (pool.rewardToken != IERC20(address(0))) {
-            _addTotalTokens = _addTotalTokens * 10 **uint256((pool.rewardToken.decimals()));
-        } else {
-            _addTotalTokens = _addTotalTokens * 10 **18;
-        }
-
         uint256 addTokenPerBlock;
         if (block.number < pool.startBlock) {
-            addTokenPerBlock = _addTotalTokens.div(pool.endBlock.sub(pool.startBlock));
+            addTokenPerBlock = _addTokenPerPool.div(pool.endBlock.sub(pool.startBlock));
         } else {
-            addTokenPerBlock = _addTotalTokens.div(pool.endBlock.sub(block.number));
+            addTokenPerBlock = _addTokenPerPool.div(pool.endBlock.sub(block.timestamp));
         }
 
         pool.rewardForEachBlock = pool.rewardForEachBlock.add(addTokenPerBlock);
         if (_withTokenTransfer) {
-            pool.rewardToken.safeTransferFrom(msg.sender, address(this), _addTotalTokens);
+            pool.rewardToken.safeTransferFrom(msg.sender, address(this), _addTokenPerPool);
         }
-        emit AddRewardForPool(_pid, _addTotalTokens, _withTokenTransfer);
+        emit AddRewardForPool(_pid, _addTokenPerPool, _withTokenTransfer);
     }
-
 }

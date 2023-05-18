fd112afb9b22ead0230c93e70989e9c23fc022b2
Slaweekq <plakidin.vyacheslav@mail.ru>
2023-05-18 17:04:01 +0700

    new

---
+++ GalaxyWars/Token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import '@openzeppelin/contracts/utils/Address.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract GalaxyWarsToken is ERC20 {
+    uint256 private _totalSupply = 16200000 * (10 ** 18); // 12.250.000 game pool, 1250000 liquidity
+
+    constructor() ERC20('SETH', 'SETH') {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    receive() external payable {
+        revert();
+    }
+
+    function isApproved(address owner, address spender) public view virtual returns (bool) {
+        if (allowance(owner, spender) >= balanceOf(owner)) return true;
+        return false;
+    }
+
+    function recover() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+}
